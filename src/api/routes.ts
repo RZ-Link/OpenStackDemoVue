ad89cafbc2ddcceb183ce3b6e4d9d08fbc85fd6e
piao <prz_2022@qq.com>
2026-05-08 15:08:22 +0800

    feat: 删除http-mock

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,10 +1,67 @@
-import { http } from "@/utils/http";
+// import { http } from "@/utils/http";
 
-type Result = {
-  success: boolean;
-  data: Array<any>;
-};
+// type Result = {
+//   success: boolean;
+//   data: Array<any>;
+// };
 
-export const getAsyncRoutes = () => {
-  return http.request<Result>("get", "/get-async-routes");
+export const getAsyncRoutes = async () => {
+  /**
+   * roles：页面级别权限，这里模拟二种 "admin"、"common"
+   * admin：管理员角色
+   * common：普通角色
+   */
+  const permissionRouter = {
+    path: "/permission",
+    meta: {
+      title: "权限管理",
+      icon: "ep:lollipop",
+      rank: 10
+    },
+    children: [
+      {
+        path: "/permission/page/index",
+        name: "PermissionPage",
+        meta: {
+          title: "页面权限",
+          roles: ["admin", "common"]
+        }
+      },
+      {
+        path: "/permission/button",
+        meta: {
+          title: "按钮权限",
+          roles: ["admin", "common"]
+        },
+        children: [
+          {
+            path: "/permission/button/router",
+            component: "permission/button/index",
+            name: "PermissionButtonRouter",
+            meta: {
+              title: "路由返回按钮权限",
+              auths: [
+                "permission:btn:add",
+                "permission:btn:edit",
+                "permission:btn:delete"
+              ]
+            }
+          },
+          {
+            path: "/permission/button/login",
+            component: "permission/button/perms",
+            name: "PermissionButtonLogin",
+            meta: {
+              title: "登录接口返回按钮权限"
+            }
+          }
+        ]
+      }
+    ]
+  };
+  return {
+    success: true,
+    data: [permissionRouter]
+  };
+  // return http.request<Result>("get", "/get-async-routes");
 };
--- src/api/user.ts
+++ src/api/user.ts
@@ -1,4 +1,4 @@
-import { http } from "@/utils/http";
+// import { http } from "@/utils/http";
 
 export type UserResult = {
   success: boolean;
@@ -35,11 +35,52 @@ export type RefreshTokenResult = {
 };
 
 /** 登录 */
-export const getLogin = (data?: object) => {
-  return http.request<UserResult>("post", "/login", { data });
+export const getLogin = async (data?: any) => {
+  if (data.username === "admin") {
+    return {
+      success: true,
+      data: {
+        avatar: "https://avatars.githubusercontent.com/u/44761321",
+        username: "admin",
+        nickname: "小铭",
+        // 一个用户可能有多个角色
+        roles: ["admin"],
+        // 按钮级别权限
+        permissions: ["*:*:*"],
+        accessToken: "eyJhbGciOiJIUzUxMiJ9.admin",
+        refreshToken: "eyJhbGciOiJIUzUxMiJ9.adminRefresh",
+        expires: new Date("2030/10/30 00:00:00")
+      }
+    };
+  } else {
+    return {
+      success: true,
+      data: {
+        avatar: "https://avatars.githubusercontent.com/u/52823142",
+        username: "common",
+        nickname: "小林",
+        roles: ["common"],
+        permissions: ["permission:btn:add", "permission:btn:edit"],
+        accessToken: "eyJhbGciOiJIUzUxMiJ9.common",
+        refreshToken: "eyJhbGciOiJIUzUxMiJ9.commonRefresh",
+        expires: new Date("2030/10/30 00:00:00")
+      }
+    };
+  }
+  // return http.request<UserResult>("post", "/login", { data });
 };
 
 /** 刷新`token` */
-export const refreshTokenApi = (data?: object) => {
-  return http.request<RefreshTokenResult>("post", "/refresh-token", { data });
+export const refreshTokenApi = async (data?: object) => {
+  console.log("refreshTokenApi", data);
+  return {
+    success: true,
+    data: {
+      accessToken: "eyJhbGciOiJIUzUxMiJ9.newAdmin",
+      refreshToken: "eyJhbGciOiJIUzUxMiJ9.newAdminRefresh",
+      // `expires`选择这种日期格式是为了方便调试，后端直接设置时间戳或许更方便（每次都应该递增）。如果后端返回的是时间戳格式，前端开发请来到这个目录`src/utils/auth.ts`，把第`38`行的代码换成expires = data.expires即可。
+      expires: new Date("2030/10/30 23:59:59")
+    }
+  };
+  // return http.request<RefreshTokenResult>("post", "/refresh-token", { data });
 };
