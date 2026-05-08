e83a84571b4921091477c79771679941938953a6
piao <prz_2022@qq.com>
2026-05-08 16:25:17 +0800

    feat: 镜像页面开发

--- src/views/image/index.vue
+++ src/views/image/index.vue
@@ -1,14 +1,37 @@
-<script setup lang="ts">
+<script setup>
+import { ref, onMounted } from "vue";
 import { getImageList } from "@/api/image";
 
 defineOptions({
   name: "image_"
 });
 
-const imageListResponse = await getImageList();
-console.log(imageListResponse);
+const imageList = ref([]);
+
+onMounted(async () => {
+  const imageListResponse = await getImageList();
+  imageList.value = [];
+  imageList.value = imageListResponse.data.images;
+});
 </script>
 
 <template>
-  <h1>image</h1>
+  <div>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>
+        <el-table :data="imageList" style="width: 100%">
+          <el-table-column prop="id" label="id" />
+          <el-table-column prop="name" label="名称" />
+          <el-table-column prop="status" label="状态" />
+          <el-table-column prop="diskFormat" label="磁盘格式" />
+          <el-table-column prop="size" label="大小（字节）" />
+          <el-table-column prop="checksum" label="校验和（MD5）" />
+          <el-table-column prop="createdAt" label="创建时间" />
+          <el-table-column prop="updatedAt" label="更新时间" />
+        </el-table>
+      </el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </div>
 </template>
